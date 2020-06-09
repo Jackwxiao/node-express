575ba05a13951f0bb23d359cf51393a8d0e61e1e
wuxiao <1291383707@qq.com>
2020-06-09 17:14:41 +0800

    install 模板引擎 ejs并使用

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const path = require('path')
 const express = require('express')
 const app =  express()
 const indexRouter = require('./routers/index')
 const userRouter = require('./routers/users')
+
+app.set('views',path.join(__dirname,'views')) // 设置存放模板文件的目录
+app.set('view engine', 'ejs') // 设置模板引擎为ejs
 
 app.use('/',indexRouter)
 app.use('/users', userRouter)
